d280e0f2457952948564c7233a7e2a5c66930c04
ORION-1971 <kandme@rambler.ru>
2025-02-25 15:20:22 +0300

    work 25/02/25 testService

---
+++ src/test/java/com/ms_account/MsAccountApplicationTest.java
@@ -0,0 +1,12 @@
+package com.ms_account;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MsAccountApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
