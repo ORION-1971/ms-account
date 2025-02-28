c1ba21fbad5e9122c5a480be94438cd45e452757
ORION-1971 <kandme@rambler.ru>
2025-03-01 00:11:41 +0300

    work 28/02/25 ExceptionHandler + Service конструктор исправлен

--- src/main/java/com/ms_account/service/AccountService.java
+++ src/main/java/com/ms_account/service/AccountService.java
@@ -3,6 +3,7 @@ package com.ms_account.service;
 import com.ms_account.entity.Account;
 import com.ms_account.exception.NotFoundException;
 import com.ms_account.repository.AccountRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,15 +13,11 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 @Transactional(readOnly = true)
 public class AccountService {
 
     private final AccountRepository repository;
-
-    @Autowired
-    public AccountService(AccountRepository repository) {
-        this.repository = repository;
-    }
 
     public List<Account> findAll() {                                             // *** ВЫВОД ВСЕХ Users ***
         return repository.findAll();
