212cd2d6d460e20fff2bba1e5c542b5e70e1c203
ORION-1971 <kandme@rambler.ru>
2025-02-28 09:30:41 +0300

    work 28/02/25 ExceptionHandler

---
+++ src/main/java/com/ms_account/exception/AccountExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.ms_account.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class AccountExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<AccountIncorrectData> handleAccountException(NotFoundException exceptiion) {
+        AccountIncorrectData data = new AccountIncorrectData();
+
+        data.setStatus(HttpStatus.NOT_FOUND.value());
+        data.setMessage(exceptiion.getMessage());                      // "Аккаунта с id - " + id + " не существует!"
+        data.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);       // 404
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<AccountIncorrectData> handleAccountException(Exception exceptiion) {
+        AccountIncorrectData data = new AccountIncorrectData();
+
+        data.setStatus(HttpStatus.BAD_REQUEST.value());
+        data.setMessage(exceptiion.getMessage());                        // "Неверные данные, введи Integer.");
+        data.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);       //400
+    }
+}
